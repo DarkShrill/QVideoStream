93a8f8f2eaa5ad000f41f445689ed8837c58eb92
Edoardo Papa <57830753+DarkShrill@users.noreply.github.com>
2026-07-14 14:56:09 +0200

    Replace VideoRendererItem with QVideoStream

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include "videorendereritem.h"
+#include "qvideostream.h"
 
 int main(int argc, char *argv[]) {
 
     QGuiApplication app(argc, argv);
 
-    qmlRegisterType<VideoRendererItem>("VideoStream", 1, 0, "VideoStream");
+    QVideoStream::registerTypes();
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
